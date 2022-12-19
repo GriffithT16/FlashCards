c9bd32bcec4599ceee9a3a36202334ee90944672
Tyler Griffith <griffitht16@gmail.com>
2022-12-19 16:26:04 -0600

    Added conditional rendering to flip between question and answer

--- src/Components/Card/CardViewer.jsx
+++ src/Components/Card/CardViewer.jsx
@@ -7,7 +7,9 @@ import React, { useState, useEffect } from 'react';
 
 export default function CardViewer(props) {
     const [cards, setCards] = useState([]);
-    const [index,setIndex] = useState(0)
+    const [index,setIndex] = useState(0);
+    const [firstIsTrue, setFirstIsTrue] = useState(false);
+
     useEffect(() => {
         fetchCards();
       }, [props.selectedCollection]);
@@ -21,8 +23,8 @@ export default function CardViewer(props) {
     }
     const handleNext = () => {
         setIndex(index+1)
-        if (index == 2){
-            setIndex(2)
+        if (index == cards.length -1){
+            setIndex(0)
         }
     }
     const handlePrev = () => {
@@ -31,23 +33,55 @@ export default function CardViewer(props) {
             setIndex(0)
         }
     }
-    const displayAnswer = () => {
-        let answer = (cards[index].definition)
-        console.log(answer)
-        alert (answer)
-    }
-
+    const handleButtonClick = () => {
+        setFirstIsTrue(!firstIsTrue);
+    };
     return (
-        <div style={{height:"15rem", width:"25rem", borderStyle:"solid", borderRadius:"1em", padding:"1em"}}>
-            {cards[index]?.word} 
-            <div style={{padding:".5em"}}>
-                <button style={{margin:".5em"}} type='submit' onClick={displayAnswer}>Answer</button>            
-                <button style={{margin:".5em"}} type='submit' onClick={handleNext}>Next</button>
-                <button style={{margin:".5em"}} type='submit' onClick={handlePrev}>Previous</button>
-            </div>        
-
-
-        </div>
-
+        <>
+            <div style={{height:"15rem", width:"25rem", borderStyle:"solid", borderRadius:"1em", padding:"1em"}}>
+                <div>{firstIsTrue ? cards[index]?.definition : cards[index]?.word}</div>
+                <div style={{padding:".5em"}}>
+                    <button style={{margin:".5em"}} type='submit' onClick={handleButtonClick}>Flip Card</button>            
+                    <button style={{margin:".5em"}} type='submit' onClick={handleNext}>Next</button>
+                    <button style={{margin:".5em"}} type='submit' onClick={handlePrev}>Previous</button>
+                </div>
+            </div>     
+        </>
     )
+    // const displayAnswer = () => {
+    //     return (
+    //         <div style={{height:"15rem", width:"25rem", borderStyle:"solid", borderRadius:"1em", padding:"1em"}}>
+    //             {cards[index]?.definition} 
+    //             <div style={{padding:".5em"}}>
+    //                 <button style={{margin:".5em"}} type='submit' onClick={displayQuestion}>Question</button>            
+    //                 <button style={{margin:".5em"}} type='submit' onClick={handleNext}>Next</button>
+    //                 <button style={{margin:".5em"}} type='submit' onClick={handlePrev}>Previous</button>
+    //             </div>     
+    //         </div>
+    //     )
+    // }
+    // const displayQuestion = () => {
+    //     return (
+    //         <div style={{height:"15rem", width:"25rem", borderStyle:"solid", borderRadius:"1em", padding:"1em"}}>
+    //             {cards[index]?.word} 
+    //             <div style={{padding:".5em"}}>
+    //                 <button style={{margin:".5em"}} type='submit' onClick={handleButtonClick}>Answer</button>            
+    //                 <button style={{margin:".5em"}} type='submit' onClick={handleNext}>Next</button>
+    //                 <button style={{margin:".5em"}} type='submit' onClick={handlePrev}>Previous</button>
+    //             </div>        
+    //         </div>
+    //     )
+    // }
+
+    // return (
+    //     <div style={{height:"15rem", width:"25rem", borderStyle:"solid", borderRadius:"1em", padding:"1em"}}>
+    //         {cards[index]?.word} 
+    //         <div style={{padding:".5em"}}>
+    //             <button style={{margin:".5em"}} type='submit' onClick={handleButtonClick}>Answer</button>            
+    //             <button style={{margin:".5em"}} type='submit' onClick={handleNext}>Next</button>
+    //             <button style={{margin:".5em"}} type='submit' onClick={handlePrev}>Previous</button>
+    //         </div>        
+    //     </div>
+
+    // )
 }
