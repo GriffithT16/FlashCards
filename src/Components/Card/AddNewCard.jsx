77f282e357d212c9de8b88baae0a87d31aa7baa1
Tyler Griffith <griffitht16@gmail.com>
2022-12-19 17:47:06 -0600

    Created AddNewCard component

---
+++ src/Components/Card/AddNewCard.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+
+
+const AddNewCard = (props) => {
+
+    const [word, setWord] = useState('');
+    const [definition, setDefinition] = useState('');
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        let newCard = {
+            word: word,
+            definition: definition,
+        };
+        // debugger;
+        console.log(newCard)
+        let response = await axios.post('http://127.0.0.1:8000/api/collections/${props.selectedCollection}/cards/', newCard);
+        if(response.status === 201){
+            return true;
+        }
+    }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Word</label>
+                <input type='text' value={word} onChange={(event) => setWord(event.target.value)} />
+            </div>
+            <div>
+                <label>Definition</label>
+                <input type='text' value={definition} onChange={(event) => setDefinition(event.target.value)} />
+            </div>
+        </form>
+
+     );
+}
+ 
+export default AddNewCard;
