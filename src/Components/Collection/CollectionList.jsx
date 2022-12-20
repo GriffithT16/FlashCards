54f9c757a8fd12cb01ba2d56c9e2e13e5da1eed6
Tyler Griffith <griffitht16@gmail.com>
2022-12-19 18:14:28 -0600

    Cleaned up coding in app.js

--- src/Components/Collection/CollectionList.jsx
+++ src/Components/Collection/CollectionList.jsx
@@ -4,18 +4,15 @@ import React, { useState, useEffect } from "react";
 function CollectionList(props) {
   return (
     <div className="">
-      <h1>View Collections</h1>
+      <h1>Collections</h1>
       {props.collections && (
         <div>
           {props.collections.map((entry) => {
-            return (
-              <div className="active">
+            return (             
                 <div className="border-box" onClick={() => props.setSelectedCollection(entry.id)} >
                   <h1>{entry.id} - </h1>
                   <h1> {entry.title}</h1>
-                </div>
-              </div>
-
+                </div>           
             );
           })}
         </div>
