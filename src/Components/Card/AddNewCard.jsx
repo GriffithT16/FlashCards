ecf3ffc470380bfcbee4a4aedf5d48bf49d08e83
Tyler Griffith <griffitht16@gmail.com>
2022-12-20 16:28:04 -0600

    Added more styling to addnewcard component

--- src/Components/Card/AddNewCard.jsx
+++ src/Components/Card/AddNewCard.jsx
@@ -1,43 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 
-
-
 const AddNewCard = (props) => {
+  const [word, setWord] = useState("");
+  const [definition, setDefinition] = useState("");
 
-    const [word, setWord] = useState('');
-    const [definition, setDefinition] = useState('');
-
-
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        let newCard = {
-            word: word,
-            definition: definition,
-        };
-        // debugger;
-        console.log(newCard)
-        let response = await axios.post(`http://127.0.0.1:8000/api/collections/${props.selectedCollection}/cards/`, newCard);
-        if(response.status === 201){
-            props.fetchCollections()
-            return true;
-        }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let newCard = {
+      word: word,
+      definition: definition,
+    };
+    // debugger;
+    console.log(newCard);
+    let response = await axios.post(
+      `http://127.0.0.1:8000/api/collections/${props.selectedCollection}/cards/`,
+      newCard
+    );
+    if (response.status === 201) {
+      props.fetchCollections();
+      return true;
     }
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Word</label>
-                <input type='text' value={word} onChange={(event) => setWord(event.target.value)} />
-            </div>
-            <div>
-                <label>Definition</label>
-                <input type='text' value={definition} onChange={(event) => setDefinition(event.target.value)} />
-            </div>
-            <button type='submit' style={{margin:"1em", color:"blue", height:"2rem", width:"8rem"}}>Create Card</button>
-        </form>
+  }
+  return (
+    <div className="border-box-card">
+      <form onSubmit={handleSubmit}>
+        <div style={{ margin: "1rem" }}>
+          <label style={{color: "whitesmoke"}}>Word: </label>
+          <input
+            type="text"
+            value={word}
+            onChange={(event) => setWord(event.target.value)}
+          />
+        </div>
+        <div style={{ margin: "1rem" }}>
+          <label style={{color: "whitesmoke"}}>Definition: </label>
+          <input
+            type="text"
+            value={definition}
+            onChange={(event) => setDefinition(event.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          style={{
+            margin: "1em",
+            color: "black",
+            height: "2rem",
+            width: "8rem",
+            backgroundColor: "red",
+            borderColor: "whitesmoke"
+          }}
+        >
+          Create Card
+        </button>
+      </form>
+    </div>
+  );
+};
 
-     );
-}
- 
 export default AddNewCard;
